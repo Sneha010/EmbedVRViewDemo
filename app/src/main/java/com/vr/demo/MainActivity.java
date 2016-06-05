e0d63ed8f216a838256a9ff683e76ff9db40bff9
Sneha <Sneha>
2016-06-05 23:02:30 +0530

    initial commit

---
+++ app/src/main/java/com/vr/demo/MainActivity.java
@@ -0,0 +1,60 @@
+package com.vr.demo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mVrImage;
+    private Button mVrImageFromRemote;
+    private Button mVrVideo;
+    private Button mVrVideoFromRemote;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mVrImage = (Button) findViewById(R.id.vrImage);
+        mVrVideo = (Button) findViewById(R.id.vrVideo);
+        mVrImageFromRemote = (Button) findViewById(R.id.vrImageRemote);
+        mVrVideoFromRemote = (Button) findViewById(R.id.vrVideoRemote);
+
+        mVrImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this , VRImageActivity.class);
+                startActivity(i);
+            }
+        });
+
+        mVrImageFromRemote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this , VRImageActivity.class);
+                startActivity(i);
+            }
+        });
+
+        mVrVideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this , VRVideoActivity.class);
+                startActivity(i);
+            }
+        });
+
+        mVrVideoFromRemote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this , VRVideoFromRemote.class);
+                startActivity(i);
+            }
+        });
+    }
+
+
+}
---
+++ app/src/main/java/com/vr/demo/VRImageActivity.java
@@ -0,0 +1,171 @@
+package com.vr.demo;
+
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Pair;
+import android.widget.Toast;
+
+import com.google.vr.sdk.widgets.pano.VrPanoramaEventListener;
+import com.google.vr.sdk.widgets.pano.VrPanoramaView;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class VRImageActivity extends AppCompatActivity {
+
+    private static final String TAG = VRImageActivity.class.getSimpleName();
+    private VrPanoramaView panoWidgetView;
+    private boolean loadImageSuccessful;
+    /** Configuration information for the panorama. **/
+    private VrPanoramaView.Options panoOptions = new VrPanoramaView.Options();
+    private ImageLoaderTask backgroundImageLoaderTask;
+
+    /** Tracks the file to be loaded across the lifetime of this app. **/
+    private Uri fileUri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vrimage);
+
+        panoWidgetView = (VrPanoramaView) findViewById(R.id.pano_view);
+        panoWidgetView.setEventListener(new ActivityEventListener());
+
+        handleIntent(getIntent());
+    }
+
+    /**
+     * Listen to the important events from widget.
+     */
+    private class ActivityEventListener extends VrPanoramaEventListener {
+        /**
+         * Called by pano widget on the UI thread when it's done loading the image.
+         */
+        @Override
+        public void onLoadSuccess() {
+            loadImageSuccessful = true;
+        }
+
+        /**
+         * Called by pano widget on the UI thread on any asynchronous error.
+         */
+        @Override
+        public void onLoadError(String errorMessage) {
+            loadImageSuccessful = false;
+            Toast.makeText(
+                    VRImageActivity.this, "Error loading pano: " + errorMessage, Toast.LENGTH_LONG)
+                    .show();
+            Log.e(TAG, "Error loading pano: " + errorMessage);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        panoWidgetView.pauseRendering();
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        panoWidgetView.resumeRendering();
+    }
+
+    @Override
+    protected void onDestroy() {
+        // Destroy the widget and free memory.
+        panoWidgetView.shutdown();
+
+        // The background task has a 5 second timeout so it can potentially stay alive for 5 seconds
+        // after the activity is destroyed unless it is explicitly cancelled.
+        if (backgroundImageLoaderTask != null) {
+            backgroundImageLoaderTask.cancel(true);
+        }
+        super.onDestroy();
+    }
+
+    class ImageLoaderTask extends AsyncTask<Pair<Uri, VrPanoramaView.Options>, Void, Boolean> {
+
+        /**
+         * Reads the bitmap from disk in the background and waits until it's loaded by pano widget.
+         */
+        @Override
+        protected Boolean doInBackground(Pair<Uri, VrPanoramaView.Options>... fileInformation) {
+            VrPanoramaView.Options panoOptions = null;  // It's safe to use null VrPanoramaView.Options.
+            InputStream istr = null;
+            if (fileInformation == null || fileInformation.length < 1
+                    || fileInformation[0] == null || fileInformation[0].first == null) {
+                AssetManager assetManager = getAssets();
+                try {
+                    istr = assetManager.open("andes.jpg");
+                    panoOptions = new VrPanoramaView.Options();
+                    panoOptions.inputType = VrPanoramaView.Options.TYPE_STEREO_OVER_UNDER;
+                } catch (IOException e) {
+                    Log.e(TAG, "Could not decode default bitmap: " + e);
+                    return false;
+                }
+            } else {
+                try {
+                    istr = new FileInputStream(new File(fileInformation[0].first.getPath()));
+                    panoOptions = fileInformation[0].second;
+                } catch (IOException e) {
+                    Log.e(TAG, "Could not load file: " + e);
+                    return false;
+                }
+            }
+
+
+            panoWidgetView.loadImageFromBitmap(BitmapFactory.decodeStream(istr), panoOptions);
+            try {
+                istr.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Could not close input stream: " + e);
+            }
+
+            return true;
+        }
+    }
+
+
+    /**
+     * Load custom images based on the Intent or load the default image. See the Javadoc for this
+     * class for information on generating a custom intent via adb.
+     */
+    private void handleIntent(Intent intent) {
+        // Determine if the Intent contains a file to load.
+        if (Intent.ACTION_VIEW.equals(intent.getAction())) {
+            Log.i(TAG, "ACTION_VIEW Intent recieved");
+
+            fileUri = intent.getData();
+            if (fileUri == null) {
+                Log.w(TAG, "No data uri specified. Use \"-d /path/filename\".");
+            } else {
+                Log.i(TAG, "Using file " + fileUri.toString());
+            }
+
+            panoOptions.inputType = intent.getIntExtra("inputType", VrPanoramaView.Options.TYPE_MONO);
+            Log.i(TAG, "Options.inputType = " + panoOptions.inputType);
+        } else {
+            Log.i(TAG, "Intent is not ACTION_VIEW. Using default pano image.");
+            fileUri = null;
+            panoOptions.inputType = VrPanoramaView.Options.TYPE_MONO;
+        }
+
+        // Load the bitmap in a background thread to avoid blocking the UI thread. This operation can
+        // take 100s of milliseconds.
+        if (backgroundImageLoaderTask != null) {
+            // Cancel any task from a previous intent sent to this activity.
+            backgroundImageLoaderTask.cancel(true);
+        }
+        backgroundImageLoaderTask = new ImageLoaderTask();
+        backgroundImageLoaderTask.execute(Pair.create(fileUri, panoOptions));
+    }
+}
